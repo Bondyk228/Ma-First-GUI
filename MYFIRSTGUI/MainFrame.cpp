20ccd7a95d08a043c25f46651ef0b5f182b04065
Славик <bondyk.slavka10@gmail.com>
2025-09-08 00:16:36 +0300

    changed event method

--- MYFIRSTGUI/MainFrame.cpp
+++ MYFIRSTGUI/MainFrame.cpp
@@ -1,27 +1,20 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
-enum 
-{
-	BtnID = 5,
-	SldrID = 6,
-	TxtID = 7
 
-};
-wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
-EVT_BUTTON(BtnID, MainFrame::OnButtonClicked)
-EVT_TEXT(TxtID, MainFrame::OnTextChanged)
-EVT_SLIDER(SldrID, MainFrame::OnSliderChanged)
-wxEND_EVENT_TABLE()
 MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
 
 {
 	wxPanel* panel = new wxPanel(this, wxID_ANY);
-
-	wxButton* button = new wxButton(panel, BtnID, "Click me for a surpirze", wxPoint(20, 20), wxDefaultSize);
-	wxSlider* slider = new wxSlider(panel, SldrID, 0, 0, 100, wxPoint(20, 70), wxSize(150, -1), wxSL_VALUE_LABEL);
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, TxtID, "Enter text here", wxPoint(20, 120), wxSize(200, -1));
+	wxButton* button = new wxButton(panel, wxID_ANY, "Click me for a surpirze", wxPoint(20, 20), wxDefaultSize);
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(20, 70), wxSize(150, -1), wxSL_VALUE_LABEL);
+	wxTextCtrl* textctrl = new wxTextCtrl(panel, wxID_ANY, "Enter text here", wxPoint(20, 120), wxSize(200, -1));
 	
+	button->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
+	slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
+	textctrl->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
+
+
 	CreateStatusBar();
 
 
