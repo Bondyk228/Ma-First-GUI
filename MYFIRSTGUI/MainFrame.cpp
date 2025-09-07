de0207245703aa94fd0be091264af9b6f4fe8a05
Славик <bondyk.slavka10@gmail.com>
2025-09-07 20:58:02 +0300

    Uh

---
+++ MYFIRSTGUI/MainFrame.cpp
@@ -0,0 +1,8 @@
+#include "MainFrame.h"
+#include <wx/wx.h>
+
+MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
+	
+{
+	// You can add more initialization code here if needed
+}
