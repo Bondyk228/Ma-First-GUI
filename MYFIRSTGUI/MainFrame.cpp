53035f45ecb780c30286fc05bc020fa890f6acc5
Pusdestroyer228 <Pusdestroyer228@LAPTOP-OWNED-BY>
2025-09-08 18:51:39 +0300

    ya pizdatiy ya ne mogu

--- MYFIRSTGUI/MainFrame.cpp
+++ MYFIRSTGUI/MainFrame.cpp
@@ -6,43 +6,29 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 
 {
 	wxPanel* panel = new wxPanel(this, wxID_ANY);
-	wxButton* button1 = new wxButton(panel, wxID_ANY, "Click me for a surpirze", wxPoint(20, 20), wxDefaultSize);
-	wxButton* button2 = new wxButton(panel, wxID_ANY, "No, click me!", wxPoint(200, 20), wxDefaultSize); 
 	
-
-
-	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
-	this->Bind(wxEVT_BUTTON, &MainFrame::AnyButtonClick, this);
-	button1->Bind(wxEVT_BUTTON, &MainFrame::Button1Click, this);
-	button2->Bind(wxEVT_BUTTON, &MainFrame::Button2Click, this);
-	
-
-
 	CreateStatusBar();
 
+	panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseButton, this);
+	panel->Bind(wxEVT_RIGHT_DOWN, &MainFrame::OnMouseButton, this);
+	panel->Bind(wxEVT_MIDDLE_DOWN, &MainFrame::OnMouseButton, this);
+	panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseMove, this);
 
 }
+void MainFrame::OnMouseMove(wxMouseEvent& event) {
+	wxPoint mousepos = event.GetPosition();
+	wxString micemvmntmsg = wxString::Format("Mouse at AXIS (%d, %d)", mousepos.x, mousepos.y);
+	wxLogStatus(micemvmntmsg);
 
-void MainFrame::OnClose(wxCloseEvent& evt)
-{
-	wxMessageBox(">:(", "mathafacka");
-	evt.Skip();
 }
-
-void MainFrame::Button1Click(wxCommandEvent& evt)
-{
-	wxMessageBox("YOU ARE A NIGGER!");
-	wxLogStatus("Surprise :3");
-	evt.Skip();
-}
-void MainFrame::Button2Click(wxCommandEvent& evt) 
-{
-	wxMessageBox("And yet ur a nigger");
-		wxLogStatus("not even a surprise, bruh");
-		evt.Skip();
-}
-void MainFrame::AnyButtonClick(wxCommandEvent& evt) 
-{
-	wxLogMessage("you pressed a button btw");
-	
+void MainFrame::OnMouseButton(wxMouseEvent& event) {
+	if (event.LeftDown()) {
+		wxLogStatus("Left Mouse Button Clicked");
+	}
+	else if (event.RightDown()) {
+		wxLogStatus("Right Mouse Button Clicked");
+	}
+	else if (event.MiddleDown()) {
+		wxLogStatus("Middle Mouse Button Clicked");
+	}
 }
--- MYFIRSTGUI/MainFrame.h
+++ MYFIRSTGUI/MainFrame.h
@@ -6,10 +6,7 @@ class MainFrame : public wxFrame
 public: 
 	MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 private:
-	void AnyButtonClick(wxCommandEvent& evt);
-	void Button1Click(wxCommandEvent& evt);
-	void Button2Click(wxCommandEvent& evt);
-	void OnClose(wxCloseEvent& evt);
-
+	void OnMouseMove(wxMouseEvent& event);
+	void OnMouseButton(wxMouseEvent& event);
 };
 
